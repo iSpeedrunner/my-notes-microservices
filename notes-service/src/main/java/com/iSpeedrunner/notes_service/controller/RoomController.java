bc9071627795121e27d0b8c862a9a28e65d15f66
Teterskyi Vladyslav <matganzovka90@gmail.com>
2026-05-29 01:56:47 +0300

    add(gateway): create gateway service

--- notes-service/src/main/java/com/iSpeedrunner/notes_service/controller/RoomController.java
+++ notes-service/src/main/java/com/iSpeedrunner/notes_service/controller/RoomController.java
@@ -5,7 +5,6 @@ import com.iSpeedrunner.notes_service.service.RoomService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
